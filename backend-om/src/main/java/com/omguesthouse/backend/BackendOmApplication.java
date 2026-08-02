aba1f7aef5b7b0b7774ef98d41f3e4d1af1d2f19
Archishmaan74 <archishmaan.upa4@gmail.com>
2026-08-03 01:36:40 +0530

    setup spring boot backend

---
+++ backend-om/src/main/java/com/omguesthouse/backend/BackendOmApplication.java
@@ -0,0 +1,13 @@
+package com.omguesthouse.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOmApplication.class, args);
+	}
+
+}
---
+++ backend-om/src/test/java/com/omguesthouse/backend/BackendOmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omguesthouse.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
