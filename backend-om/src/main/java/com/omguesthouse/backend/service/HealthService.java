86f985bc9b671d7064ac33293daf1c82e9693fd3
Ishita-13 <ishukumari162@gmail.com>
2026-08-30 15:36:52 +0530

    created health api for checking backend

--- backend-om/src/main/java/com/omguesthouse/backend/service/HealthService.java
+++ backend-om/src/main/java/com/omguesthouse/backend/service/HealthService.java
@@ -1,4 +1,10 @@
 package com.omguesthouse.backend.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class HealthService {
+    public String getHealthStatus() {
+        return "Backend is running!";
+    }
 }
