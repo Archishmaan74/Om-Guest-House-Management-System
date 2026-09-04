792ad731d5160b31422a167970e12a66dc5c3f10
Archishmaan74 <archishmaan.upa4@gmail.com>
2026-09-04 23:46:58 +0530

    created a api response dto and used for health check api

--- backend-om/src/main/java/com/omguesthouse/backend/controller/HealthController.java
+++ backend-om/src/main/java/com/omguesthouse/backend/controller/HealthController.java
@@ -1,5 +1,6 @@
 package com.omguesthouse.backend.controller;
 
+import com.omguesthouse.backend.dto.ApiResponse;
 import com.omguesthouse.backend.service.HealthService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/health")
 public class HealthController {
+
   private final HealthService healthService;
+
   public HealthController(HealthService healthService){
       this.healthService= healthService;
   }
+
     @GetMapping
-    public String health(){
-       return healthService.getHealthStatus();
+    public ApiResponse<String> health(){
+       String healthStatus = healthService.getHealthStatus();
+       return new ApiResponse<>(
+               true,
+                "Health check successful",
+                healthStatus
+       );
     }
 }
