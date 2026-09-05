aa59eb07ee0e538710757d77020437e4d414d46c
Archishmaan74 <archishmaan.upa4@gmail.com>
2026-09-05 11:31:23 +0530

    feature-added internal server error status in exception handler

--- backend-om/src/main/java/com/omguesthouse/backend/exception/GlobalExceptionHandler.java
+++ backend-om/src/main/java/com/omguesthouse/backend/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.omguesthouse.backend.exception;
 
 import com.omguesthouse.backend.dto.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -8,11 +9,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ApiResponse<?> handleException(Exception e) {
-        return new ApiResponse<>(
-                false,
-                e.getMessage(),
-                null
-        );
+    public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
+        return ResponseEntity.internalServerError()
+                .body(new ApiResponse<>(false,
+                        e.getMessage(),
+                        null));
     }
 }
